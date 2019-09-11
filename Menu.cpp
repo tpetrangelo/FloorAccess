cd86fb07fb039c5f1b2c14dda008a0e11f94d40f
Tom Petrangelo <tdpetrangelo@gmail.com>
2019-09-11 18:09:59 -0400

    Started making comments in Menu.cpp, Menu.h is done

--- Menu.cpp
+++ Menu.cpp
@@ -1,3 +1,4 @@
+//Pre-processor directives
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,13 +10,18 @@
 #include "Guest.h"
 #include "PrintResource.h"
 
+//Initialization of static vectors for each type of resizable object
 static std::vector<Company> newCompany;
 static std::vector<Guest> newGuest;
 static std::vector<CompanyEmployee> newCompanyEmployee;
 static std::vector<BuildingEmployee> newBuildingEmployee;
+
+//Static counters for resizable occupants, used for printing out number of occupants in a building
 static int guestCount{ 0 };
 static int companyEmployeeCount{ 0 };
 static int buildingEmployeetCount{ 0 };
+
+//Reference variables used for adding to occupant counters
 static int& guestCountRef = guestCount;
 static int& companyEmployeetCountRef = companyEmployeeCount;
 static int& buildingEmployeetCountRef = buildingEmployeetCount;
@@ -24,10 +30,12 @@ static int& buildingEmployeetCountRef = buildingEmployeetCount;
 
 void Menu::userStartMenu()
 {
+	//Outputs start menu, askes user to input the number of floors in a building
 	printView.runStartMenu();
 	std::cin >> numberOfFloors;
 	std::cout << std::endl;
 
+	//Checks if user-input is less than 0 or equal to 1. Outputs and error message if so
 	while (numberOfFloors == 1 || numberOfFloors < 0) {
 		
 		std::cout << "There must be a postive number of floors, greater than one , please re-enter: ";
@@ -35,53 +43,89 @@ void Menu::userStartMenu()
 		std::cout << std::endl;
 	}
 	
+	//Creates room for numberOfFloors-1 companies
 	newCompany.reserve(numberOfFloors-1);
+
+	//Creates a Building obejct with the number of floors based on user input
 	Building building(numberOfFloors);
-	Building::numberOfFloorsLeft.resize(numberOfFloors);
-	Building::numberOfFloorsLeft[0] = 0;
+
+	//Resizes numberOfFloorsLeft vector based on user input 
+	Building::numberOfFloorsLeft.reserve(numberOfFloors);
+
+	//Check this
+	//Building::numberOfFloorsLeft[0] = 0;
 }
 
 
 void Menu::userMenu()
 {
 	while (1) {
+
+		//Outputs initial options for user
+		//Input 1 will bring user to company menu
+		//Input 2 will bring user to occupant menu
 		printView.printBuildingStatus();
 		std:: cin >> input; 
 		std::cout << std::endl;
-		//company menu
+
+		//Main Menu Input 1: Brings user to company menu
 		if (input == 1) {
+
+			//Outputs options for user based on company menu
 			printView.printCompanyStatus();
 			std:: cin >> input;
 			std::cout << std::endl;
-			//add a company
+			
+			//Company Menu Input 1: Add a company to the directory
 			if (input == 1) {
+
+				//Increments the company counter
 				companyCount++;
 
-				
+				//Asks user to input company name
 				printView.newCompanyName();
 				std::cin >> companyName;
+
+				//Prints out open floors left for user to pick for the new company
 				building.openFloorsLeft();
 				std::cout << "Please enter your company's first floor: ";
 				std::cin >> firstFloor;
 				std::cout << "Please enter your company's last floor: ";
 				std::cin >> lastFloor;
 				std::cout << std::endl;
+
+				//Validates user-input to ensure floor selection was acceptable
 				if (building.validateCompanyFloors(firstFloor, lastFloor) == true) {
+
+					//If floor input is okay, push new company on to the newCompany vector
 					newCompany.push_back(Company(companyName, firstFloor, lastFloor));
+
+					//Output confirming company has been added to directory
 					printView.companyAddition(companyName, firstFloor, lastFloor);
 					std::cout << std::endl;
 				}
 				else {
+					
+					//User input for company floors was not valid
 					std::cout << companyName << " could not be added to the directory!" << std::endl << std::endl;
+					
+					//Decrement number of companies in company count
 					companyCount--;
 				}
+
+				//Start back at the beginning of the menu screen
 				continue;
 			}
-			//view all companies
+
+			//Company Menu Input 2: Outputs all the companies and their respective floors
 			else if (input == 2) {
+
+				//If there are no companies in the directory, output error
 				if (newCompany.empty() == true) {
 					std::cout << "There are no companies in the directory!" << std::endl << std::endl;
 				}
+
+				//Print out all the company information using range based for loop
 				else {
 					std::cout << "Company Directory" << std::endl;
 					for (auto& company : newCompany) {
@@ -90,7 +134,7 @@ void Menu::userMenu()
 				}
 
 			}
-			//error validation
+			//Company Menu: If 1 or 2 was not entered, output error message and have user re-input
 			else {
 				while (input != 1 && input != 2) {
 					std::cout << "Invalid Input. Please re-enter" << std::endl;
@@ -98,27 +142,42 @@ void Menu::userMenu()
 				}
 			}
 		}
-		//Occupant Menu
+		//Main Menu Input 2: Brings user to Occupant Menu
 		else if (input == 2) {
+
+			//Print out options for user in Occupant menu
 			printView.printOccupantStatus();
 			std:: cin >> input; 
 			std::cout << std::endl;
-			//Add an Occupant
+
+
+			//Occupant Menu Input 1: Add an Occupant
 			if (input == 1) {
+
+				//Output options for user based on Occupant Menu
 				printView.printOccupantOptions();
 				std:: cin >> input; 
 				std::cout << std::endl;
-				//Guest menu
+
+				//Occupant Options Input 1: Guest menu
 				if (input == 1) {
+					
+					//Print out possible options for user based on Guest object
 					guest.printOccupantMenu();
 					std:: cin >> input; 
 					std::cout << std::endl;
-					//add a guest
+
+
+					//Guest Menu Input 1: Add a guest
 					if (input == 1) {
+
+						//Add to guest counter
 						guestCountRef++;
+
+						//Create space on newGuest vector for an addtional Guest
 						newGuest.reserve(guestCountRef);
 
-					
+						//Asks user to input details about the new guest
 						std::cout << "Please enter your guest's first name: ";
 						std::cin >> firstName;
 						std::cout << "Please enter your guest's last name: ";
@@ -126,24 +185,36 @@ void Menu::userMenu()
 						std::cout << "Please enter your guest's age: ";
 						std::cin >> age;
 						std::cout << std::endl;
+
+						//Adds guest to directory
 						newGuest.push_back(Guest(firstName, lastName, age));
+
+						//Output confirming the new guest has been added to teh directory
 						printView.guestAddition(firstName, lastName, age);
+
+						//Bring user back to the Building menu
 						continue;
 					}
-					//view all current guests
+					//Guest Menu Input 2: View all guests in directory
 					else if (input == 2) {
+
+						//If there are no current guests, let user know
 						if (newGuest.empty() == true) {
 							std::cout << "There are no guests in the directory!" << std::endl << std::endl;
 						}
+
+						//Output all guest information using range based for loop
 						else {
 							std::cout << "Guest Directory" << std::endl;
 							for (auto& guests : newGuest) {
 								guests.printStatus();
 							}
 						}
+
+						//Bring user back to the Building menu
 						continue;
 					}
-					//error checking
+					//Guest Menu: If 1 or 2 was not entered, output error message and have user re-input
 					else {
 						while (input != 1 && input != 2 && input != 3) {
 							std::cout << "Invalid Input. Please re-enter\n ";
@@ -152,7 +223,7 @@ void Menu::userMenu()
 						}
 					}
 				}
-				//company employee menu
+				//Occupant Options Input 2: Guest menu
 				else if (input == 2) {
 					compEmployee.printOccupantMenu();
 					std::cin >> input; 
@@ -296,15 +367,4 @@ void Menu::userMenu()
 
 		}
 	}
-}
-
-void Menu::printStatus()
-{
-	std::cout << "Placeholder" << std::endl;
-}
-
-void Menu::printOccupantMenu()
-{
-	std::cout << "Placeholder" << std::endl;
-
 }
--- Menu.h
+++ Menu.h
@@ -1,3 +1,4 @@
+//Pre-processor directives
 #pragma once
 #include "Building.h"
 #include "BuildingEmployee.h"
@@ -5,17 +6,21 @@
 #include "CompanyEmployee.h"
 #include "Guest.h"
 #include "PrintResource.h"
+
+//Interface for Menu class
 class Menu
 {
 public:
 
+	//userStartMenu creates a bulding object based on the number of floors input by the user
 	void userStartMenu();
+
+	//userMenu is the main driver for the program, allowing the user to fill a bulding's directory with companies and occupants
 	void userMenu();
 
-	virtual void printStatus();
-	virtual void printOccupantMenu();
-
 private:
+
+	//Composition creating objects that will interract with each other inside of Menu.cpp
 	Building building;
 	Guest guest;
 	PrintResource printView;
@@ -23,18 +28,37 @@ private:
 	BuildingEmployee buildEmpyloyee;
 	Company company;
 	
+	//Initialization of integer variables that are private data members of the above composition objects
+	
+	//Building
 	int numberOfFloors{ 0 };
+	
+	//Menu
 	int input{ 0 };
+	
+	//Menu
 	int companyCount{ 0 };
+	
+	//Guest
 	int age{ 0 };
+	
+	//Company
 	int firstFloor{ 0 };
 	int lastFloor{ 0 };
-	int id{ 0 };
 
+	//Initialization of string variables that are private data members of the above composition objects
+	
+	//Building Employee
 	std::string position;
+	
+	//Occupant
 	std::string firstName;
 	std::string lastName;
+	
+	//Company
 	std::string companyName;
+	
+	//Company Employee
 	std::string companyEmployed;
 
 
