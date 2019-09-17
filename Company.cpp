d1fa612389e1e057efd7701db23f7438a0433b94
Thomas Petrangelo <thpetran@syr.edu>
2019-09-17 10:04:40 -0400

    Fixed a spacing issue on Company.cpp

--- Company.cpp
+++ Company.cpp
@@ -78,7 +78,6 @@ void Company::printCompanyInfo()
 	else {
 		std::cout << getCompanyName() << " Occupies Floors: " << getStartingFloor() << " through " << getEndingFloor() << std::endl;
 	}
-	std::cout << std::endl;
 	std::cout << "----------------------------------------" << std::endl;
 
 }
